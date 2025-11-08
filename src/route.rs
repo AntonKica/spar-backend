f2796f34cc106937931da57d2a7b857a52564603
Anton Kica <antonkica@pm.me>
2025-11-08 20:12:06 +0100

    addded asset get for code

--- src/route.rs
+++ src/route.rs
@@ -31,72 +31,10 @@ impl GeneralRoute for OtherRotes {
     fn routes() -> Scope {
         web::scope("")
             .service(asset_list)
-    }
-}
-#[get("/")]
-pub async fn role_list(
-    data: web::Data<AppState>,
-) -> impl Responder {
-    match RoleService::list(&data.db).await {
-        Ok(data) => HttpResponse::Ok().json(ApiResponse::new(data)),
-        Err(e) => e.error_response()
-    }
-}
-#[get("/{code}")]
-pub async fn role_get(
-    data: web::Data<AppState>,
-    path: Path<String>
-) -> impl Responder {
-    let code = path.into_inner();
-    match RoleService::get_by_code(&data.db, code).await {
-        Ok(data) => HttpResponse::Ok().json(ApiResponse::new(data)),
-        Err(e) => e.error_response()
+            .service(asset_get)
     }
 }
 
-#[get("/")]
-pub async fn application_list(
-    data: web::Data<AppState>,
-) -> impl Responder {
-    match ApplicationService::list(&data.db).await {
-        Ok(data) => HttpResponse::Ok().json(ApiResponse::new(data)),
-        Err(e) => e.error_response()
-    }
-}
-
-#[get("/{code}")]
-pub async fn application_get(
-    data: web::Data<AppState>,
-    path: Path<String>
-) -> impl Responder {
-    let code = path.into_inner();
-    match ApplicationService::get_by_code(&data.db, code).await {
-        Ok(data) => HttpResponse::Ok().json(ApiResponse::new(data)),
-        Err(e) => e.error_response()
-    }
-}
-
-#[get("/")]
-pub async fn it_system_list(
-    data: web::Data<AppState>,
-) -> impl Responder {
-    match ITSystemService::list(&data.db).await {
-        Ok(data) => HttpResponse::Ok().json(ApiResponse::new(data)),
-        Err(e) => e.error_response()
-    }
-}
-
-#[get("/{code}")]
-pub async fn it_system_get(
-    data: web::Data<AppState>,
-    path: Path<String>
-) -> impl Responder {
-    let code = path.into_inner();
-    match ITSystemService::get_by_code(&data.db, code).await {
-        Ok(data) => HttpResponse::Ok().json(ApiResponse::new(data)),
-        Err(e) => e.error_response()
-    }
-}
 #[get("/asset/")]
 pub async fn asset_list(
     data: web::Data<AppState>,
@@ -106,6 +44,29 @@ pub async fn asset_list(
         r#"SELECT * FROM asset"#,
     )
         .fetch_all(&data.db)
+        .await;
+    match query_result {
+        Ok(res) => {
+            HttpResponse::Ok().json(serde_json::json!({ "status": "ok", "data": res }))
+        }
+        Err(err) => {
+            let message = format!("{:?}", err);
+            HttpResponse::Ok().json(serde_json::json!({ "status": "failed", "error": message}))
+        }
+    }
+}
+
+#[get("/asset/{asset}")]
+pub async fn asset_get(
+    data: web::Data<AppState>,
+    path: Path<String>
+) -> impl Responder {
+    let asset = path.into_inner();
+    let query_result = sqlx::query_as!(
+        AssetModel,
+        r#"SELECT * FROM asset"#,
+    )
+        .fetch_one(&data.db)
         .await;
     match query_result {
         Ok(res) => {
